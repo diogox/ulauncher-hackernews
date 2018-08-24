e56496b0f7d55c21bcb6b58ebc1838d10a5a6a4e
Diogo Xavier <diogoxavierpinto@gmail.com>
2018-08-24 23:44:01 +0100

    Add threading to speed up api request & Remove need for Haxor.py

--- hn.py
+++ hn.py
@@ -1,17 +1,13 @@
 from hackernews import HackerNews
 from preferences import Preferences
 
+from threading import Thread
+from Queue import Queue
 import datetime
 
 class HN:
 
     _preferences = None
-    _api = None
-
-    def __init__(self):
-
-        # Initialize HackerNews API
-        self._api = HackerNews()
 
     def set_preferences(self, preferences):
 
@@ -30,18 +26,57 @@ class HN:
         total_number = items_per_page * page_number
 
         # Get the ids
-        ids = self._api.top_stories(limit=int(total_number))
+        ids_api_endpoint = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+        ids = request_api(ids_api_endpoint)
 
         # Get stories
         end_id = total_number
         start_id = end_id - items_per_page
         
         top = []
+        queue = Queue()
+        for _x in range(5):
+            story_api_endpoit = 'https://hacker-news.firebaseio.com/v0/item/'
+            worker = HN_Worker(story_api_endpoit, queue, top)
+            worker.start()
+        
         for index in range(start_id, end_id):
-            story = self._api.get_item( ids[index] )
-            top.append(story)
+            queue.put(ids[index])
 
+        queue.join()
         return top
 
-def get_page_number(starts_at, number_of_items_per_page):
-    return (int(starts_at)/int(number_of_items_per_page)) + 1
+class HN_Worker(Thread):
+
+    def __init__(self, download_link, queue, items):
+        Thread.__init__(self)
+        self._queue = queue
+        self._url = download_link
+        self._items = items
+    
+    def run(self):
+
+        while True:
+            story_id = self._queue.get()
+
+            try:
+                #Request API
+                item_info = request_api('%s%s.json' % (self._url, story_id))
+                self._items.append( Story(item_info) )
+            finally:
+                self._queue.task_done()
+
+def request_api(url):
+    import requests
+    response = requests.get(url)
+    return response.json()
+
+class Story:
+    def __init__(self, info):
+        self.item_id = info['id']
+        self.title = info['title']
+        self.by = info['by']
+        self.descendants = info['descendants']
+        self.url = info['url']
+        self.time = info['time']
+        self.score = info['score']
